1cae5429157702323391ce9d7c82dc6ffc2f380c
devarajsingh <devarajsingh5@gmail.com>
2020-05-24 19:56:53 +0530

    +

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,17 +9,28 @@ import { WINDOW } from 'src/app/service/window.service';
 })
 export class HomeComponent implements OnInit {
 
+
   @ViewChild('scroll') rt: ElementRef;
   constructor(private renderer: Renderer2, 
     @Inject(DOCUMENT) private document: Document,
     @Inject(WINDOW) private window: Window) {
    }
 
+   scrollValue = 0;
+
   ngOnInit(): void {
     this.renderer.listen('window', 'scroll', (event)=>{
       // debugger
       const offset = this.window.pageYOffset || this.document.documentElement.scrollTop || this.document.body.scrollTop || 0;
     console.log(offset);
+    if(this.scrollValue > offset) {
+      console.log("scroll up");
+    } else {
+      console.log("scroll down");
+      
+    }
+    this.scrollValue = offset;
+
     } );
   }
 
